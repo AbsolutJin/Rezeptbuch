d77542e0e63b343fe3f4fdbe0297eead290f953b
AbsolutJin <rudolf.adam@outlook.de>
2023-04-02 14:42:51 +0200

    Initial commit

---
+++ src/main/java/com/rudolf/Rezeptbuch/RezeptbuchApplication.java
@@ -0,0 +1,13 @@
+package com.rudolf.Rezeptbuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezeptbuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezeptbuchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rudolf/Rezeptbuch/RezeptbuchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudolf.Rezeptbuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezeptbuchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
